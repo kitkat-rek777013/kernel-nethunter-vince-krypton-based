5a6eae01ce700711bb1152e26b99602e70a6ec8e
kitkat-rek777013 <rremix87@gmail.com>
2019-06-25 09:44:06 +0300

    Upload all files from local repository

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#10 SMP PREEMPT Sat Jun 22 18:21:19 MSK 2019"
+#define LINUX_COMPILE_BY "kitkat"
+#define LINUX_COMPILE_HOST "vince"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.140-KaliKryPtoN-🔥PWR-0.4+"
